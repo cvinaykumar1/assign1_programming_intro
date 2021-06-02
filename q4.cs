728973bbe45ca220c2dae7e9f8c6de027d85351e
Vinay kumar C <vinaykumarc@usf.edu>
2021-06-02 19:07:20 -0400

    q4 and q5

---
+++ q4.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace assign1_programming_intro
+{
+    class q4
+    {
+        static void Main(string[] args)
+        {
+            int array_size;
+            Console.WriteLine("How many integers you want to input?");
+            array_size = Convert.ToInt32(Console.ReadLine());
+            int[] nums = new int[array_size];
+            for (int i = 0; i < array_size; i++)
+            {
+                Console.WriteLine("Enter element {0}", i);
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            q4 PivotIndex = new q4();
+            try
+            {
+                Console.WriteLine(PivotIndex.PivotIndex(nums));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public int PivotIndex(int[] nums)
+        {
+            int total_sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                total_sum += nums[i];
+            }
+            int sum = 0;
+            for (int i = 0; i < nums.Length - 1; ++i)
+            {
+                if (2 * sum + nums[i] == total_sum)
+                {
+                    return i;
+                }
+                sum += nums[i];
+
+            }
+            return -1;
+
+        }
+    }
+}
---
+++ q5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace assign1_programming_intro
+{
+    class q3
+    {
+        static void Main(string[] args)
+        {
+            string word_1, word_2;
+            Console.WriteLine("Input word 1?");
+            word_1 = Console.ReadLine();
+            Console.WriteLine("Input word 2?");
+            word_2 = Console.ReadLine();
+
+            q3 MergeAlternately = new q3();
+            try
+            {
+                Console.WriteLine(MergeAlternately.MergeAlternately(word_1, word_2));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public string MergeAlternately(string word_1, string word_2)
+        {
+            string final_word = "";
+
+            for (int i = 0; i < word_1.Length || i < word_2.Length; i++)
+            {
+                if (i < word_1.Length)
+                {
+                    final_word += word_1[i];
+                }
+                if (i < word_2.Length)
+                {
+                    final_word += word_2[i];
+                }
+            }
+            return final_word;
+
+        }
+    }
+}
